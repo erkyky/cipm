33a1c2f65ac0cce4f0903909ac8c80e68cee1cf1
Eric Wu <ericwu56@gmail.com>
2015-03-24 09:04:17 -0400

    using db

--- cipm/__init__.py
+++ cipm/__init__.py
@@ -1,6 +1,29 @@
 import flask
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config.from_object('config')
+
+
+def get_db():
+    db = getattr(flask.g, '_database', None)
+    if db is None:
+        db = flask.g._database = sqlite3.connect('cipm.db')
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(flask.g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 from cipm import views
